7e6f9dd9c593fbbadb2e015c1c05b63bc759d384
Hank Pan <dhankb@gmail.com>
2025-05-11 01:27:08 +0800

    1657. Determine if Two Strings Are Close

---
+++ py/1657_Determine_if_Two_Strings_Are_Close.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+from typing import *
+from collections import OrderedDict
+from collections import deque
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        freq1, freq2 = [0] * 26, [0] * 26
+        for c in word1:
+            freq1[ord(c) - ord("a")] += 1
+        for c in word2:
+            freq2[ord(c) - ord("a")] += 1
+
+        for i in range(26):
+            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+                return False
+
+        freq1.sort()
+        freq2.sort()
+
+        for i in range(26):
+            if freq1[i] != freq2[i]:
+                return False
+
+        return True
+
+
+if __name__ == "__main__":
+    # solution = Solution()
+
+    sys.exit(0)
